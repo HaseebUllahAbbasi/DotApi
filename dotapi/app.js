29a3a8bfa529bf3c5a864d700bc8b3e810b49e93
Haseeb U <haseebabbasi00@gmail.com>
2021-12-16 13:54:59 +0500

    cors added

--- dotapi/app.js
+++ dotapi/app.js
@@ -1,7 +1,11 @@
 const express = require('express') 
 const app = new express()
+const cors = require('cors')
 app.use(express.json())
+
 const personRoutes = require('./Routers/PersonRoutes')
+
+app.use(cors())
 app.use(personRoutes)
 
 module.exports = app;
